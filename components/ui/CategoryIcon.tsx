51c0ebd9c7476365e59e1a3ed2a48a5eebdcf39f
unknown <nietsniep@gmail.com>
2024-11-26 18:06:28 -0600

    cap 662

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,12 +1,17 @@
+"use client"
 import { Category } from "@prisma/client"
 import Image from "next/image"
 import Link from "next/link"
+import { useParams } from "next/navigation"
 
 type CategoryIconProps = {
     category: Category
 }
 
 const CategoryIcon = ({category} : CategoryIconProps) => {
+  const params = useParams<{algomas:string}>();
+  console.log(params);
+  
   return (
     <div >
         <div className="w-48, h-48 relative">
